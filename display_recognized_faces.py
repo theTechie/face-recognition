8c846572a923c7268fcd7b8458cfb052bd851812
Gagan <gagan@Gagans-MacBook-Pro.local>
2018-11-20 16:12:22 +0530

    use helper functions

--- display_recognized_faces.py
+++ display_recognized_faces.py
@@ -1,6 +1,7 @@
 import face_recognition
 from PIL import Image, ImageDraw
 from pathlib import Path
+import recognize_face
 
 know_path = Path("data/known_people")
 known_images = list(know_path.glob('*.jpeg'))
@@ -8,11 +9,7 @@ known_images = list(know_path.glob('*.jpeg'))
 known_face_encodings = []
 known_face_names = []
 
-for image_to_recognize in known_images:
-  image = face_recognition.load_image_file(str(image_to_recognize))
-  face_encoding = face_recognition.face_encodings(image)[0]
-  known_face_encodings.append(face_encoding)
-  known_face_names.append(image_to_recognize.name)
+known_faces = [recognize_face.image_to_known_face(str(image_path), image_path.name) for image_path in known_images]
 
 print('I just learned to recognize %d persons... \n' % len(known_images))
 
@@ -25,9 +22,11 @@ output_path = Path("data/output")
 
 for image_to_identify in unknown_images:
     unknown_image = face_recognition.load_image_file(str(image_to_identify))
-    face_locations = face_recognition.face_locations(unknown_image)
-    face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+    # face_locations = face_recognition.face_locations(unknown_image)
+    # face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
 
+    detected_faces = recognize_face.recognize(known_faces, unknown_image)
+    
     # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
     # See http://pillow.readthedocs.io/ for more about PIL/Pillow
     pil_image = Image.fromarray(unknown_image)
@@ -35,17 +34,7 @@ for image_to_identify in unknown_images:
     draw = ImageDraw.Draw(pil_image)
 
     # Loop through each face found in the unknown image
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
-        name = "Unknown"
-
-        # If a match was found in known_face_encodings, just use the first one.
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
-
+    for name, (top, right, bottom, left) in detected_faces:
         # Draw a box around the face using the Pillow module
         draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
 
