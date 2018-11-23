6ba7fb496fec80e37048e524308f45bc92572415
theTechie <gagan.blr@gmail.com>
2018-11-23 12:14:05 +0530

    web service

---
+++ web_service.py
@@ -0,0 +1,154 @@
+# This is a _very simple_ example of a web service that recognizes faces in uploaded images.
+# Upload an image file and it will check if the image contains a picture of Barack Obama.
+# The result is returned as json. For example:
+#
+# $ curl -XPOST -F "file=@obama2.jpg" http://127.0.0.1:5001
+#
+# Returns:
+#
+# {
+#  "face_found_in_image": true,
+#  "is_picture_of_obama": true
+# }
+#
+# This example is based on the Flask file upload example: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+
+# NOTE: This example requires flask to be installed! You can install it with pip:
+# $ pip3 install flask
+
+import math
+import os
+import os.path
+import pickle
+import face_recognition
+from sklearn import neighbors
+from flask import Flask, jsonify, request, redirect
+from pathlib import Path
+from PIL import Image
+import requests
+from io import BytesIO, StringIO
+import base64
+from imageio import imread
+from base64 import decodestring
+import re
+
+# You can change this to any folder on your system
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+
+        # if 'file' not in request.files:
+        #     return redirect(request.url)
+
+        image_data = re.sub('^data:image/.+;base64,', '', str(request.data))
+
+        print('printing converted image data --------')
+        print(image_data)
+        
+        #Image.open(BytesIO(base64.b64decode(str)))
+        file = BytesIO(base64.b64decode(str))
+        
+
+        # file = request.files['file']
+
+        # file = imread(BytesIO(base64.b64decode(request.data)))
+
+        # if file.filename == '':
+        #     return redirect(request.url)
+
+        if file:
+            # b = BytesIO(file)
+            # im = Image.open(b)
+            # The image file seems valid! Detect faces and return the result.
+            # print(im)
+            return detect_faces_in_image(file)
+
+    # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method="POST" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Upload">
+    </form>
+    '''
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def predict(X_img, knn_clf=None, model_path=None, distance_threshold=0.6):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+
+    :param X_img: path to image to be recognized
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    """
+    if knn_clf is None and model_path is None:
+        raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
+
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # Load image file and find face locations
+    X_face_locations = face_recognition.face_locations(X_img)
+
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(X_img, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("Unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+def detect_faces_in_image(file_stream):
+    # Pre-calculated face encoding of Obama generated with face_recognition.face_encodings(img)
+    print("Looking for faces...")
+    
+    # Find all people in the image using a trained classifier model
+    # Note: You can pass in either a classifier file name or a classifier model instance
+    img = face_recognition.load_image_file(file_stream)
+    predictions = predict(img, model_path="models/trained_knn_model_1.clf")
+
+    face_found = False
+
+    faces_found = []
+    # Print results on the console
+    for name, (top, right, bottom, left) in predictions:
+        print("- Found {} at ({}, {})".format(name, left, top))
+        faces_found.append({'name': name, 'location': (top, right, bottom, left)})
+        face_found = True
+
+    # Return the result as json
+    result = {
+        "face_found_in_image": face_found,
+        "is_picture_of": faces_found
+    }
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
